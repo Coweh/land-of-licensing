e92af4bc210b196b362ea5517c22f507156c2df4
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-24 10:23:42 -0700

    Changed logo splash screen fade color, and it now uses tweening to fade instead of multiple sprites

--- source/Scenes/Main menu/Script.gd
+++ source/Scenes/Main menu/Script.gd
@@ -1,21 +1,21 @@
 extends Node
 
 # Declare variables for the nodes that will be used in this script.
-onready var fade = $"../Foreground/Fade"
+onready var tween = $"../Foreground/Tween"
+onready var foreground_rect = $"../Foreground/ForegroundRect"
 onready var game_logo = $"../Foreground/Game Logo"
 onready var sih_logo = $"../Foreground/SIH Logo"
 
-# Wait for a moment to pass, queue free the Shapes in Hats logo, wait for another moment, make the game's logo appear, make it disappear, and then
 func _ready():
+	# Wait for a moment to pass, queue free the Shapes in Hats logo, wait for another moment, make the game's logo appear, then queue it free.
+	# The logos are freed from the scene tree instead of simply hiding themselves because they're no longer needed in the scene.
 	yield(get_tree().create_timer(2.5), "timeout")
 	sih_logo.queue_free()
 	yield(get_tree().create_timer(1), "timeout")
 	game_logo.show()
 	yield(get_tree().create_timer(2.5), "timeout")
 	game_logo.queue_free()
-	yield(get_tree().create_timer(1), "timeout")
-	fade.play()
-
-# Queue free the fade node once it isn't needed anymore.
-func _on_Fade_animation_finished():
-	fade.queue_free()
+	yield(get_tree().create_timer(1.5), "timeout")
+	# Use tweening to fade the foreground out.
+	tween.interpolate_property(foreground_rect, "modulate", foreground_rect.color, Color(10.0, 10.0, 10.0, 0), 1)
+	tween.start()
